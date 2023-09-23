6ddca5d275e6d8c2c510664fb5bf6032708aba1a
Danilo Altheman <daltheman@gmail.com>
2023-09-22 21:49:02 -0300

    Initial Commit

---
+++ BibleReader/BibleReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibleReaderApp.swift
+//  BibleReader
+//
+//  Created by Danilo Altheman on 22/09/23.
+//
+
+import SwiftUI
+
+@main
+struct BibleReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
